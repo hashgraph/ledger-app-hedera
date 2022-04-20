6802fd30efec76046bfc5984302f8e9f9c8ed034
Ryan Leckey <ryan@launchbadge.com>
2022-04-19 22:45:45 -0700

    fix: use regular memset for clearing amount format buffer
    Signed-off-by: Ryan Leckey <ryan@launchbadge.com>

--- src/hedera_format.c
+++ src/hedera_format.c
@@ -2,21 +2,17 @@
 
 #include "hedera_format.h"
 
-// if explicit_bzero is not defined, provide with memset
-// enables unit testing
-#ifndef explicit_bzero
-#define explicit_bzero(buf, size) memset(buf, 0, size)
-#endif
-
 char* hedera_format_tinybar(uint64_t tinybar) {
     return hedera_format_amount(tinybar, 8);
 }
 
+#define BUF_SIZE 32
+
 char* hedera_format_amount(uint64_t amount, uint8_t decimals) {
-    static const int BUF_SIZE = 32;
     static char buf[BUF_SIZE];
 
-    explicit_bzero(buf, BUF_SIZE);
+    // NOTE: format of amounts are not sensitive
+    memset(buf, 0, BUF_SIZE);
 
     int i = 0;
 
